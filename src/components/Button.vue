25eefe43eb1747d1624584e5f121dbf0450bb464
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-12 14:59:34 +0100

    Create button component

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="$emit('btn-click')">{{ text }}</button>
+</template>
+
+<script>
+export default {
+  name: 'ButtonElement',
+  props: {
+    text: String,
+  },
+}
+</script>
+
+<style>
+button {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+</style>
