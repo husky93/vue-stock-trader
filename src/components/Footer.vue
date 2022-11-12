2c32c3d4a57a9ad26ede1642e01bf51c7aa2ec5c
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-12 14:29:26 +0100

    Create Footer component template

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer>
+    Copyright &copy; <a href="https://github.com/husky93">husky93</a>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterElement',
+}
+</script>
+
+<style></style>
