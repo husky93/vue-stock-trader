bd050a9d2fec5b57d20286ef70d5444cf2f0bff3
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-12 14:55:44 +0100

    Greate InfoGroup component

---
+++ src/components/InfoGroup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="info-group">
+    {{ label }}: <span>{{ value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoGroup',
+  props: {
+    label: String,
+    value: String,
+  },
+}
+</script>
+
+<style scoped>
+span {
+  font-weight: 600;
+}
+</style>
