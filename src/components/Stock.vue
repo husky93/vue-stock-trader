d7e15c48b9c5bd93437421aa831d08c78c76a7b9
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-13 12:18:15 +0100

    Create Stock component template and display all stocks

---
+++ src/components/Stock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <h4>{{ name }}</h4>
+    <div>
+      Price: <span>{{ price }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StockElement',
+  props: ['name', 'price'],
+}
+</script>
+
+<style></style>
--- src/views/StocksView.vue
+++ src/views/StocksView.vue
@@ -1,7 +1,22 @@
 <template>
   <main class="stocks m-auto">
     <h3>Stocks</h3>
+    <Stock
+      v-for="stock in $store.state.stocksAvailable"
+      :key="stock.id"
+      :name="stock.name"
+      :price="stock.price"
+    />
   </main>
 </template>
 
+<script>
+import Stock from '../components/Stock.vue'
+export default {
+  name: 'StocksView',
+  components: {
+    Stock,
+  },
+}
+</script>
 <style></style>
