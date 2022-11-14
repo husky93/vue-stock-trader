1597ce7847565039d60a27f47fba15bb68e49295
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-14 12:36:27 +0100

    Get stock data inside PortfolioItem component

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ stockObject.name }}</div>
+</template>
+
+<script>
+export default {
+  name: 'PortfolioItem',
+  props: ['stock'],
+  data() {
+    return {
+      stockObject: this.$store.state.stocksAvailable[this.stock.id],
+    }
+  },
+}
+</script>
+
+<style></style>
--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -1,5 +1,19 @@
 <template>
   <main class="portfolio m-auto">
     <h3>Portfolio</h3>
+    <PortfolioItem
+      v-for="stock in $store.state.stocksOwned"
+      :stock="stock"
+      :key="`${stock.id}-portfolio`"
+    />
   </main>
 </template>
+
+<script>
+import PortfolioItem from '../components/PortfolioItem.vue'
+export default {
+  components: {
+    PortfolioItem,
+  },
+}
+</script>
