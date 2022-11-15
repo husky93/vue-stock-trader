e8558201d465ad6385be296c5bdb57b53ef91e0a
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-15 17:31:32 +0100

    Create border glow effect on adjacent cards

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="card" @mousemove="handleMouseMove">
-    <div class="card-content">
+  <div class="card">
+    <div class="card-border"></div>
+    <div class="card-bg"></div>
+    <div class="content-wrapper">
       <h4>{{ name }}</h4>
       <div>
         Price: <span>{{ price + '$' }}</span>
@@ -66,16 +68,6 @@ export default {
       this.$emit('stock-bought')
       this.count = 0
     },
-    handleMouseMove(e) {
-      const { currentTarget: target } = e
-
-      const rect = target.getBoundingClientRect(),
-        x = e.clientX - rect.left,
-        y = e.clientY - rect.top
-
-      target.style.setProperty('--mouse-x', `${x}px`)
-      target.style.setProperty('--mouse-y', `${y}px`)
-    },
   },
 }
 </script>
@@ -88,7 +80,6 @@ export default {
   height: 260px;
   width: 300px;
   border-radius: 10px;
-  padding: 24px;
   position: relative;
 }
 
@@ -96,12 +87,8 @@ export default {
   opacity: 1;
 }
 
-.card::before {
-  background: radial-gradient(
-    800px circle at var(--mouse-x) var(--mouse-y),
-    rgba(255, 255, 255, 0.2),
-    transparent 40%
-  );
+.card::before,
+.card > .card-border {
   border-radius: inherit;
   content: '';
   opacity: 0;
@@ -111,11 +98,41 @@ export default {
   position: absolute;
   top: 0px;
   width: 100%;
-  z-index: 2;
 }
 
-.card-content {
-  position: relative;
+.card::before {
+  background: radial-gradient(
+    800px circle at var(--mouse-x) var(--mouse-y),
+    rgba(255, 255, 255, 0.2),
+    transparent 40%
+  );
   z-index: 3;
+}
+
+.card > .card-border {
+  background: radial-gradient(
+    400px circle at var(--mouse-x) var(--mouse-y),
+    rgba(255, 255, 255, 0.3),
+    transparent 40%
+  );
+  z-index: 1;
+}
+
+.card > .card-bg {
+  height: calc(100% - 2px);
+  width: calc(100% - 2px);
+  background-color: rgb(23, 23, 23);
+  border-radius: inherit;
+  margin: 1px;
+  z-index: 2;
+  position: relative;
+}
+
+.content-wrapper {
+  position: absolute;
+  padding: 24px;
+  left: 0;
+  top: 0;
+  z-index: 10;
 }
 </style>
--- src/views/StocksView.vue
+++ src/views/StocksView.vue
@@ -1,7 +1,7 @@
 <template>
   <main class="stocks m-auto">
     <h3>Stocks</h3>
-    <div class="container">
+    <div class="stocks-container" @mousemove="handleMouseMove">
       <Stock
         @stock-bought="resetCounter"
         v-for="stock in $store.state.stocksAvailable"
@@ -11,6 +11,7 @@
         :id="stock.id"
         :maxAmount="stock.maxAmount"
         :reset="reset"
+        ref="stocks"
       />
     </div>
   </main>
@@ -33,24 +34,37 @@ export default {
       this.reset = true
       await setTimeout(() => (this.reset = false), 0)
     },
+    handleMouseMove(e) {
+      for (const card of document.getElementsByClassName('card')) {
+        const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top
+        card.style.setProperty('--mouse-x', `${x}px`)
+        card.style.setProperty('--mouse-y', `${y}px`)
+      }
+    },
   },
   created() {
     this.$store.dispatch('changeMaxAmount')
   },
 }
 </script>
-<style scoped>
-main {
+<style>
+.stocks {
   text-align: center;
   display: flex;
   flex-direction: column;
   align-items: center;
 }
-.container {
+.stocks-container {
   display: flex;
   flex-wrap: wrap;
   gap: 8px;
   max-width: 916px;
   width: calc(100% - 20px);
+}
+
+.stocks-container:hover > .card > .card-border {
+  opacity: 1;
 }
 </style>
