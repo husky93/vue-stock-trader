41df6422d9f81cef31e4436c0f293c478507bab3
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-15 17:50:00 +0100

    Add card element to portfolio page

--- src/views/PortfolioView.vue
+++ src/views/PortfolioView.vue
@@ -4,21 +4,50 @@
     <div v-if="$store.state.stocksOwned.length === 0">
       <h4>You dont own any stocks yet! It's time to buy some.</h4>
     </div>
-    <div v-if="$store.state.stocksOwned.length > 0">
-      <PortfolioItem
+    <div
+      v-if="$store.state.stocksOwned.length > 0"
+      class="portfolio-container"
+      @mousemove="handleMouseMove"
+    >
+      <Card
         v-for="stock in $store.state.stocksOwned"
-        :stock="stock"
-        :key="`${stock.id}-portfolio`"
-      />
+        :key="`${stock.id}-portfolio-card`"
+      >
+        <PortfolioItem :stock="stock" :key="`${stock.id}-portfolio-item`" />
+      </Card>
     </div>
   </main>
 </template>
 
 <script>
 import PortfolioItem from '../components/PortfolioItem.vue'
+import Card from '../components/Card.vue'
+import { cardsHoverMixin } from '../app/mixins'
 export default {
   components: {
     PortfolioItem,
+    Card,
   },
+  mixins: [cardsHoverMixin],
 }
 </script>
+
+<style>
+.portfolio {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.portfolio-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  max-width: 916px;
+  width: calc(100% - 20px);
+}
+
+.portfolio-container:hover > .card > .card-border {
+  opacity: 1;
+}
+</style>
