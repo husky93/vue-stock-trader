18d25dd99a185e142b0b98361e1c61f03951da11
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-16 21:28:07 +0100

    Style the progress bar

--- src/components/Goal.vue
+++ src/components/Goal.vue
@@ -11,9 +11,42 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 progress {
   width: 75%;
-  height: 1rem;
+  height: 16px;
+  margin-bottom: 24px;
+  position: relative;
+  background: #eee;
+  border-radius: 100px;
+}
+
+progress::-moz-progress-bar {
+  border-radius: 100px;
+  background: linear-gradient(
+    to right,
+    var(--primary-color),
+    var(--secondary-color)
+  );
+}
+
+progress::-webkit-progress-bar {
+  background: #eee;
+  border-radius: 100px;
+}
+
+progress::-webkit-progress-value {
+  border-radius: 100px;
+  background: linear-gradient(
+    to right,
+    var(--primary-color),
+    var(--secondary-color)
+  );
+}
+.goal {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 24px;
 }
 </style>
